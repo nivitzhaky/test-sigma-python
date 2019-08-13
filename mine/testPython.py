ad85cc1327f32fd18e6bdf8249b8791f178efb08
nivitzhaky <niv.itzhaky@gmail.com>
2019-08-14 01:11:20 +0300

    two

--- mine/testPython.py
+++ mine/testPython.py
@@ -1,3 +1,23 @@
+import boto3
+ddb = boto3.client("dynamodb")
+
 def handler(event, context):
+    data = []
+    try:
+        data = ddb.get_item(
+            TableName='test',
+            Key={'my':{'S':'my'}}
+        )
+    except BaseException as e:
+        print(e)
+        raise(e)
     
-    return {"message": "Successfully executed"}
+    return data
+    try:
+        data = ddb.put_item(
+            TableName='test',
+            Item={'att':{'S':'val'},'my':{'S':'my'}}
+        )
+    except BaseException as e:
+        print(e)
+        raise(e)
