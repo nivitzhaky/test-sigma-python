fb52e2be84569f4a097d7bb428d6e8c040dd0b0e
nivitzhaky <niv.itzhaky@gmail.com>
2019-08-14 18:40:26 +0300

    cm

--- mine/lambda.js
+++ mine/lambda.js
@@ -1,3 +1,5 @@
+const {WebhookClient, Suggestion} = require('dialogflow-fulfillment');
+
 exports.handler = function(event, context, callback) {
     
     callback(null, {"message": "Successfully executed"});
